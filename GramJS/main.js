0287b5a98aa6e6b33cbcc7c89181dac24c711057
rodukov <electroneuphoria@gmail.com>
2022-06-18 15:12:44 +0300

    gramjs

---
+++ GramJS/main.js
@@ -0,0 +1,24 @@
+const { TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
+const input = require("input"); // npm i input
+
+const apiId = 1;
+const apiHash = "1";
+const stringSession = new StringSession(""); // fill this later with the value from session.save()
+
+(async () => {
+  console.log("Loading interactive example...");
+  const client = new TelegramClient(stringSession, apiId, apiHash, {
+    connectionRetries: 5,
+  });
+  await client.start({
+    phoneNumber: async () => await input.text("Please enter your number: "),
+    password: async () => await input.text("Please enter your password: "),
+    phoneCode: async () =>
+      await input.text("Please enter the code you received: "),
+    onError: (err) => console.log(err),
+  });
+  console.log("You should now be connected.");
+  console.log(client.session.save()); // Save this string to avoid logging in again
+  await client.sendMessage("me", { message: "Hello!" });
+})();
