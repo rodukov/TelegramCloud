f75e61edb235a383073040b6ef93c6548a0d96d2
rodukov <electroneuphoria@gmail.com>
2022-07-20 19:51:00 +0300

    applcation

--- application/src/router/index.js
+++ application/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
+  { path: '/login', name: 'enter', component: () => import('../views/EnterView.vue') },
   { path: '/about', name: 'about', component: () => import('../views/AboutView.vue') }
 ]
 
---
+++ application/src/views/EnterView.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script>
+export default {
+    name: 'enter',
+    mounted() {
+        document.title = "Login into TelegramCloud"
+    }
+}
+</script>
