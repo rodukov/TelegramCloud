239a0aee0a2c7e82743497ef6ef3142602b129f1
rodukov <electroneuphoria@gmail.com>
2022-07-20 19:54:00 +0300

    backend

--- TelegramCloudAPI/core/TelegramCloudConfig.py
+++
@@ -1,4 +0,0 @@
-PATH_TO_LOGIN = "session/login.json"
-PATH_TO_SAVE = "session/TelegramCloudSession"
-PATH_TO_CHANNEL = "session/channel.json"
-CHANNEL_DESCRIPTION = "Convenient file storage TelegramCloud https://github.com/rodukov/TelegramCloud"
